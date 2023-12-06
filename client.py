825975057e98af9b3887803709c752a7878a966c
Mohammad Al Ramis <MohammadRRamis@outlook.com>
2023-12-07 00:22:16 +0300

    Fix client.py to use socket communication for CRUD operations

--- client.py
+++ client.py
@@ -1,26 +1,5 @@
-from hashRing import ring, removeNode
-import time
-
-
-
-# we need to ensure the client is aware of the node information 
-# and that the HashRing is utilized correctly for distributing keys across the nodes
-
-# Initialize the client with node information
-hr = ring()
-
-# # Remove "node2" from the hash ring
-# print(hr.get_nodes())
-# # removeNode('node2')
-# removeNode('node1')
-# print(hr.get_nodes())
-# print(hr["1"].get("1"))
-
-"""
-Issue:
-    In case of an update or change in the ring, the hr needs to be updated for the client 
-
-"""
+import socket
+import pickle
 
 while True:
     print("Available actions: create, read, update, delete, exit")
@@ -28,26 +7,32 @@ while True:
 
     if action == 'exit':
         break
-    
+
     if action in ['create', 'read', 'update', 'delete']:
         key = input("Enter key: ")
 
         if action == 'create' or action == 'update':
-            hr = ring()
             value = input("Enter value: ")
-            if action == 'create':
-                hr[key].set(key,value)
-                print(hr[key])
-  
-                
-        #     elif action == 'update':
-        #         client.update(key, value)
-        elif action == 'read':
-            hr = ring()
-            result = hr[key].get(key)
+
+        # Create a socket object
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Define the port on which you want to connect
+        port = 12345
+
+        # Connect to the server on local computer
+        s.connect(('127.0.0.1', port))
+
+        # Send the request to the server
+        request = {'key': key, 'action': action, 'value': value}
+        s.send(pickle.dumps(request))
+
+        if action == 'read':
+            # Receive the result from the server
+            result = pickle.loads(s.recv(1024))
             print(result)
-            print(hr[key])
-        # elif action == 'delete':
-        #     client.delete(key)
+
+        # Close the connection
+        s.close()
     else:
         print("Invalid action. Please try again.")
--- hashRing.py
+++ hashRing.py
@@ -2,33 +2,90 @@ from uhashring import HashRing
 import threading
 import redis
 import time
+import socket
+import pickle
+
+
+def start_election(nodename):
+    """
+    Start an election from the given node.
+    """
+    # Get all nodes with higher IDs
+    higher_nodes = [node for node in nodes if node > nodename]
+
+    if higher_nodes:
+        # Send election message to all nodes with higher IDs
+        for node in higher_nodes:
+            send_election_message(node)
+    else:
+        # If there are no nodes with higher IDs, this node becomes the coordinator
+        become_coordinator(nodename)
+
+
+def send_election_message(nodename):
+    """
+    Send an election message to the given node.
+    """
+    # If the node responds, it will start its own election
+    if nodes[nodename]["status"] == "active":
+        start_election(nodename)
+    else:
+        # If the node does not respond, remove it from the election
+        nodes[nodename]["status"] = "inactive"
+
+
+def become_coordinator(nodename):
+    """
+    Become the coordinator and notify all other nodes.
+    """
+    # Set the current node as the coordinator
+    nodes[nodename]["status"] = "coordinator"
+
+    # Notify all other nodes
+    for node in nodes:
+        if node != nodename:
+            send_coordinator_message(node)
+
+
+def send_coordinator_message(nodename):
+    """
+    Send a coordinator message to the given node.
+    """
+    # If the node is active, update its coordinator
+    if nodes[nodename]["status"] == "active":
+        nodes[nodename]["coordinator"] = nodename
 
 
 nodes = {
-    'node1': {
-        'hostname': 'localhost',
-        'instance': redis.StrictRedis(host='localhost', port=6379), #replica on port 6370
-        'vnodes': 40,
-        'port': 6379,
-        'status': 'active',
-        'replica': redis.StrictRedis(host='localhost', port=6370),
-        
+    "node1": {
+        "hostname": "localhost",
+        "instance": redis.StrictRedis(
+            host="localhost", port=6379
+        ),  # replica on port 6370
+        "vnodes": 40,
+        "port": 6379,
+        "status": "active",
+        "replica": redis.StrictRedis(host="localhost", port=6370),
+        "coordinator": "node2"
     },
-    'node2': {
-        'hostname': 'localhost',
-        'instance': redis.StrictRedis(host='localhost', port=6378), 
-        'vnodes': 40,
-        'port': 6378,
-        'status': 'active',
-        # 'replica': redis.StrictRedis(host='localhost', port=6371),
+    "node2": {
+        "hostname": "localhost",
+        "instance": redis.StrictRedis(host="localhost", port=6378),
+        "vnodes": 40,
+        "port": 6378,
+        "status": "active",
+        'replica': redis.StrictRedis(host='localhost', port=6371),
+        "coordinator": "node2"
     },
 }
 
 
 hr = HashRing(nodes)
 
+
 def ring():
     return hr
+
 
 def nodesInfo():
     return nodes
@@ -38,6 +95,8 @@ def nodesInfo():
 In case of a failure, the keys-values in the failed nodes needs to be 
 distributed to other nodes in the ring using the "replication" of the failed node
 """
+
+
 def removeNode(nodename):
     # Create a temporary HashRing without the node being removed
     temp_nodes = nodes.copy()
@@ -45,24 +104,25 @@ def removeNode(nodename):
     temp_hr = HashRing(temp_nodes)
 
     try:
-        replica = nodes[nodename].get('replica')
+        replica = nodes[nodename].get("replica")
         keys = replica.keys()
         for key in keys:
             value = replica.get(key)
             new_node_name = temp_hr.get_node(key)
             if new_node_name != nodename:
-                nodes[new_node_name].get('instance').set(key,value)
+                nodes[new_node_name].get("instance").set(key, value)
     except AttributeError:
         print("their are not keys in this node")
 
-    nodes[nodename]['status'] = 'inactive'
+    nodes[nodename]["status"] = "inactive"
     hr.remove_node(nodename)
 
     print(f"Node {nodename} removed successfully.")
 
+
 def addNode(nodename):
-    if nodename in nodes and nodes[nodename]['status'] == "inactive":
-        nodes[nodename]['status'] = "active"
+    if nodename in nodes and nodes[nodename]["status"] == "inactive":
+        nodes[nodename]["status"] = "active"
         hr.add_node(nodename)
 
     # temp_nodes = nodes.copy()
@@ -84,7 +144,7 @@ def addNodeRedistributeData(source_node, target_node, hr):
     """
     Move data from the source node to the target node based on the updated hash ring.
     """
-    source_instance = nodes[source_node]['instance']
+    source_instance = nodes[source_node]["instance"]
     keys = source_instance.keys()
 
     for key in keys:
@@ -92,42 +152,39 @@ def addNodeRedistributeData(source_node, target_node, hr):
         correct_node = hr.get_node(key)
         if correct_node == target_node:
             value = source_instance.get(key)
-            nodes[target_node].get('instance').set(key,value)
+            nodes[target_node].get("instance").set(key, value)
             # nodes[target_node]['instance'].set(key, value)
             source_instance.delete(key)
 
 
-
-def getNodeAfter(hr, nodename): 
+def getNodeAfter(hr, nodename):
     """
     Get the next node in the hash ring after the specified node.
     """
     sorted_nodes = sorted(hr.get_nodes())
     current_index = sorted_nodes.index(nodename)
 
-    # the mod (%) is used to "wrap around" if the index goes beyond the length of the list. 
+    # the mod (%) is used to "wrap around" if the index goes beyond the length of the list.
     next_index = (current_index + 1) % len(sorted_nodes)
     return sorted_nodes[next_index]
 
 
-
-
 # Regular Health Checks: Periodically ping each node to check its status.
-# Handling Failures: If a node fails to respond, consider it as unavailable 
+# Handling Failures: If a node fails to respond, consider it as unavailable
 # and proceed with failure handling, remove it from the HashRing and redistributing its data.
 def check_node_health(node_instance, nodename):
     try:
         if node_instance.ping():
-            if nodes[nodename]['status'] == 'inactive':
+            if nodes[nodename]["status"] == "inactive":
                 print(f"{nodename} is back online. Re-adding to HashRing.")
                 addNode(nodename)
             return True
     except redis.exceptions.ConnectionError:
-        if nodes[nodename]['status'] == 'active':
+        if nodes[nodename]["status"] == "active":
             print(f"{nodename} is down. Marking as inactive.")
             removeNode(nodename)
+            start_election(nodename)
         return False
-
 
 
 def periodic_health_check(interval=5):
@@ -135,32 +192,63 @@ def periodic_health_check(interval=5):
         for nodename in list(nodes.keys()):
             node_info = nodes.get(nodename)
             if node_info:
-                check_node_health(node_info['instance'], nodename)
+                check_node_health(node_info["instance"], nodename)
 
         time.sleep(interval)
 
 
-def readReplica(nodename):
-    instance = nodes[nodename].get('instance')
-        # In case of failure, switch to a replica
-    try:
-        keys = instance.keys()
-    except ConnectionError:
-        # Handle the failure - switch to replica
-        replica = nodes[nodename].get('replica')
-        if replica:
-            print(f"Primary node {nodename} is down. Switching to replica.")
-            instance = replica
-            keys = instance.keys()
-            return keys, instance
-        else:
-            print(f"No replica available for node {nodename}.")
-            return
+# def readReplica(nodename):
+#     instance = nodes[nodename].get("instance")
+#     # In case of failure, switch to a replica
+#     try:
+#         keys = instance.keys()
+#     except ConnectionError:
+#         # Handle the failure - switch to replica
+#         replica = nodes[nodename].get("replica")
+#         if replica:
+#             print(f"Primary node {nodename} is down. Switching to replica.")
+#             instance = replica
+#             keys = instance.keys()
+#             return keys, instance
+#         else:
+#             print(f"No replica available for node {nodename}.")
+#             return
+
 
 # Start the periodic health check in a separate thread
 health_check_thread = threading.Thread(target=periodic_health_check, args=(30,))
 health_check_thread.start()
 
 
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Define the port on which you want to connect
+port = 12345
 
+# Bind to the port
+s.bind(('127.0.0.1', port))
+
+# Put the socket into listening mode
+s.listen(5)
+
+while True:
+    # Establish a connection with the client
+    c, addr = s.accept()
+
+    # Receive the client's request
+    request = pickle.loads(c.recv(1024))
+
+    # Process the request
+    hr = ring()
+    key = request['key']
+    action = request['action']
+    value = request.get('value')  # 'value' might not be in the request
+    if action == 'create':
+        hr[key].set(key, value)
+    elif action == 'read':
+        result = hr[key].get(key)
+        c.send(pickle.dumps(result))
+
+    # Close the connection with the client
+    c.close()
